b659d6d3e9cb7ce2cdb610c75365248f587550e3
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-07-18 23:13:08 +0530

    db connection

---
+++ config/database.js
@@ -0,0 +1,20 @@
+//mongoose import 
+const mongoose = require("mongoose");
+
+//url config m h so 
+require("dotenv").config();
+
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+  
+    }
+  
+    ).then(console.log("DB CONNECTION SUCCESS")).catch((error) => {
+        console.log('DB connection issues');
+        console.error(error);
+        process.exit(1);
+    })
+}
